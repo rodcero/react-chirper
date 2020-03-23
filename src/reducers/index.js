9b93020639dd4f67bbc230765d7ea3803d1c7f11
ernie <2838152+rodcero@users.noreply.github.com>
2020-03-23 15:25:33 -0700

    feature: setup logger middleware

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
-import { combinedReducers } from 'redux';
+import { combineReducers } from 'redux';
 import authedUser from './authedUser';
 import users from './users';
 import tweets from './tweets';
 
-export default combinedReducers(authedUser, users, tweets);
+export default combineReducers(authedUser, users, tweets);
