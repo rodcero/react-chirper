e07e1d9e248a92a5d63c746d357d1c7152b524fe
ernie <2838152+rodcero@users.noreply.github.com>
2020-03-25 19:56:27 -0700

    feature: added routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import LoadingBar from 'react-redux-loading';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
+import Nav from './Nav';
+import NewTweet from './NewTweet';
+import TweetPage from './TweetPage';
 
 class App extends Component {
   componentDidMount() {
@@ -12,10 +16,21 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <LoadingBar />
-        {this.props.loading === true ? null : <Dashboard />}
-      </div>
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          <div className="container">
+            <Nav />
+            {this.props.loading === true ? null : (
+              <div>
+                <Route path="/" exact component={Dashboard} />
+                <Route path="/tweet/:id" component={TweetPage} />
+                <Route path="/new" component={NewTweet} />
+              </div>
+            )}
+          </div>
+        </Fragment>
+      </Router>
     );
   }
 }
