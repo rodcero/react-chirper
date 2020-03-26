145ec1913a62a1ac4222289c3c87297086d4d4cb
ernie <2838152+rodcero@users.noreply.github.com>
2020-03-25 19:56:16 -0700

    feature: added tweet actions

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,6 +1,55 @@
+import { saveLikeToggle, saveTweet } from '../utils/api';
+import { showLoading, hideLoading } from 'react-redux-loading';
+
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS';
+export const TOGGLE_TWEET = 'TOGGLE_TWEET';
+export const ADD_TWEET = 'ADD_TWEET';
+
+const addTweet = tweet => {
+  return {
+    type: ADD_TWEET,
+    tweet,
+  };
+};
+
+export const handleAddTweet = (text, replyingTo) => {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+
+    dispatch(showLoading());
+
+    return saveTweet({
+      text,
+      author: authedUser,
+      replyingTo,
+    })
+      .then(tweet => dispatch(addTweet(tweet)))
+      .then(() => dispatch(hideLoading()));
+  };
+};
 
 export const receiveTweets = tweets => ({
   type: RECEIVE_TWEETS,
   tweets,
 });
+
+const toggleTweet = ({ id, authedUser, hasLiked }) => {
+  return {
+    type: TOGGLE_TWEET,
+    id,
+    authedUser,
+    hasLiked,
+  };
+};
+
+export const handleToggleTweet = info => {
+  return dispatch => {
+    dispatch(toggleTweet(info));
+
+    return saveLikeToggle(info).catch(e => {
+      console.warn('Error in handleToggleTweet: ', e);
+      dispatch(toggleTweet(info));
+      alert('There was and error liking the tweet. Please try again.');
+    });
+  };
+};
